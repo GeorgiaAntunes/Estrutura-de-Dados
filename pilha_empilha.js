b9e14e068a0f3cfffd8600e8df89664a3536b12a
georgia.antunes <georgiaantunes@outlook.com>
2024-04-30 16:01:21 -0300

    definindo estrutura pilha

---
+++ pilha_empilha.js
@@ -0,0 +1,17 @@
+let stack = [];
+
+stack.push(1);
+stack.push(2);
+stack.push(3);
+stack.push(4);
+
+console.log(stack);
+
+stack.pop();
+
+console.log("Steak after first element removed "+ stack);
+
+stack.pop();
+
+console.log("Steak after second element removed "+ stack);
+
